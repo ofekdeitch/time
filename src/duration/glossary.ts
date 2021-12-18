a956b457dab7cea08b96fdf464f5d98d873af64b
Ofek Deitch <62507549+ofekdeitch-fiverr@users.noreply.github.com>
2021-12-18 14:03:17 +0200

    Deconstruct (#7)

---
+++ src/duration/glossary.ts
@@ -0,0 +1,10 @@
+export interface DeconstructedDuration {
+    milliseconds: number;
+    seconds: number;
+    minutes: number;
+    hours: number;
+    days: number;
+    weeks: number;
+    months: number;
+    years: number;
+}
