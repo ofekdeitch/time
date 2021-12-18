222831dea0cf398f893b9685150672f897550e80
Ofek Deitch <62507549+ofekdeitch-fiverr@users.noreply.github.com>
2021-12-18 11:33:42 +0200

    [Setup] publish step (#4)

--- src/test/spec.ts
+++ src/test/spec.ts
@@ -3,7 +3,7 @@ import { Greeter } from '../index';
 describe('Greeting', () => {
     
     it('greets', () => {
-        expect(Greeter('Ofek')).toEqual("Hello Ofek");
+        expect(Greeter('Ofek')).toEqual("Hello Ofek!!!!");
     })
 
 });
