af65448a50d1e0db655060dbad1204959a521908
Ofek Deitch <62507549+ofekdeitch-fiverr@users.noreply.github.com>
2021-12-18 14:35:58 +0200

    Remove it.only (#8)

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,10 @@ module.exports = {
         "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
         "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     ],
+    plugins: ["no-only-tests"],
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+        "no-only-tests/no-only-tests": "error",
     },
 };
